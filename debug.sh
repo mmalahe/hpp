f6a881869b0b20dfc1446c423ddb31934b54a55d
Michael Malahe <mmalahe@gmail.com>
2017-09-11 15:36:44 +0200

    Create build folders if they don't exist.

--- debug.sh
+++ debug.sh
@@ -1,3 +1,4 @@
+mkdir -p build-debug
 cd build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cores=$(grep -c ^processor /proc/cpuinfo)
--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
+mkdir -p build-release
 cd build-release
 cmake -DCMAKE_BUILD_TYPE=Release ..
-#~ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
 cores=$(grep -c ^processor /proc/cpuinfo)
 make -j $cores
 cd ..
