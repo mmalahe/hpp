16dee4fb10266d534b1168af877146de127780b4
Michael Malahe <mmalahe@gmail.com>
2017-09-20 14:36:17 +0200

    Cleared up copyright status of aligned_alloc.

--- include/hpp/stdlibReplacements.h
+++ include/hpp/stdlibReplacements.h
@@ -10,12 +10,31 @@
 #include <hpp/config.h>
 
 /**
- * @brief The stdlib function aligned_alloc
+ * @brief The standard library function aligned_alloc
  * @param size
  * @param align
- * @details From the llvm implementation.
+ * @details This is taken directly from the GCC-4.9 implementation of _mm_malloc,
+ * found in gmm_malloc.h. The copyright notice at the top of gmm_malloc.h is
+ * reproduced below.
+ * 
+ * Copyright (C) 2004-2014 Free Software Foundation, Inc.
+   This file is part of GCC.
+   GCC is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+   GCC is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+   You should have received a copy of the GNU General Public License and
+   a copy of the GCC Runtime Library Exception along with this program;
+   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+   <http://www.gnu.org/licenses/>.
  */
-/// @fixme Check how this license propagates or do own implementation.
 #ifndef HPP_HAVE_ALIGNED_ALLOC
 static __inline__ void* 
 aligned_alloc (size_t size, size_t align)
