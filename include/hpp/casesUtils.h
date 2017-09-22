62f700fb7749757a62fc9b06306dc5f4dc5b13a5
Michael Malahe <mmalahe@gmail.com>
2017-09-22 16:17:22 +0200

    Started on orientation generator.

--- include/hpp/casesUtils.h
+++ include/hpp/casesUtils.h
@@ -11,29 +11,30 @@
 #include <hpp/tensor.h>
 #include <functional>
 #include <stdexcept>
+#include <memory>
 
 namespace hpp 
 {
 
 // Simple shear experiment
 template <typename U>
-hpp::Tensor2<U> simpleShearDeformationGradient(U t, U shear_rate) {
-    hpp::Tensor2<U> F = hpp::identityTensor2<U>(3);
+Tensor2<U> simpleShearDeformationGradient(U t, U shear_rate) {
+    Tensor2<U> F = identityTensor2<U>(3);
     F(0,1) = shear_rate*t;
     return F;
 }
 
 template <typename U>
-hpp::Tensor2<U> simpleShearVelocityGradient(U t, U shear_rate) {
-    hpp::Tensor2<U> L(3,3);
+Tensor2<U> simpleShearVelocityGradient(U t, U shear_rate) {
+    Tensor2<U> L(3,3);
     L(0,1) = shear_rate;
     return L;
 }
 
 // Simple compression experiment
 template <typename U>
-hpp::Tensor2<U> simpleCompressionDeformationGradient(U t, U comp_rate) {
-    hpp::Tensor2<U> F(3,3);
+Tensor2<U> simpleCompressionDeformationGradient(U t, U comp_rate) {
+    Tensor2<U> F(3,3);
     F(0,0) = std::exp(-0.5*comp_rate*t);
     F(1,1) = std::exp(-0.5*comp_rate*t);
     F(2,2) = std::exp(1.0*comp_rate*t);
@@ -41,8 +42,8 @@ hpp::Tensor2<U> simpleCompressionDeformationGradient(U t, U comp_rate) {
 }
 
 template <typename U>
-hpp::Tensor2<U> simpleCompressionVelocityGradient(U t, U comp_rate) {
-    hpp::Tensor2<U> L(3,3);
+Tensor2<U> simpleCompressionVelocityGradient(U t, U comp_rate) {
+    Tensor2<U> L(3,3);
     L(0,0) = -0.5*comp_rate;
     L(1,1) = -0.5*comp_rate;
     L(2,2) = 1.0*comp_rate;
@@ -51,8 +52,8 @@ hpp::Tensor2<U> simpleCompressionVelocityGradient(U t, U comp_rate) {
 
 // Plane strain compression experiment
 template <typename U>
-hpp::Tensor2<U> planeStrainCompressionDeformationGradient(U t, U comp_rate) {
-    hpp::Tensor2<U> F(3,3);
+Tensor2<U> planeStrainCompressionDeformationGradient(U t, U comp_rate) {
+    Tensor2<U> F(3,3);
     F(0,0) = std::exp(1.0*comp_rate*t);
     F(1,1) = 1.0;
     F(2,2) = std::exp(-1.0*comp_rate*t);
@@ -60,8 +61,8 @@ hpp::Tensor2<U> planeStrainCompressionDeformationGradient(U t, U comp_rate) {
 }
 
 template <typename U>
-hpp::Tensor2<U> planeStrainCompressionVelocityGradient(U t, U comp_rate) {
-    hpp::Tensor2<U> L(3,3);
+Tensor2<U> planeStrainCompressionVelocityGradient(U t, U comp_rate) {
+    Tensor2<U> L(3,3);
     L(0,0) = 1.0*comp_rate;
     L(2,2) = -1.0*comp_rate;
     return L;
@@ -69,16 +70,41 @@ hpp::Tensor2<U> planeStrainCompressionVelocityGradient(U t, U comp_rate) {
 
 // Null experiment
 template <typename U>
-hpp::Tensor2<U> staticDeformationGradient(U t) {
-    hpp::Tensor2<U> F = hpp::identityTensor2<U>(3);
+Tensor2<U> staticDeformationGradient(U t) {
+    Tensor2<U> F = identityTensor2<U>(3);
     return F;
 }
 
 template <typename U>
-hpp::Tensor2<U> staticVelocityGradient(U t) {
-    hpp::Tensor2<U> L(3,3);
+Tensor2<U> staticVelocityGradient(U t) {
+    Tensor2<U> L(3,3);
     return L;
 }
+
+/**
+ * @class OrientationGenerator
+ * @author Michael Malahe
+ * @date 22/09/17
+ * @file casesUtils.h
+ * @brief An abstract class for reproducibly generating a sequence of orientations.
+ */
+template <typename U>
+class OrientationGenerator {
+    public:
+        virtual void generateNext(Tensor2<U>& rotMatrix) = 0;
+        void generateNext(EulerAngles<U>& angles) {};
+        virtual ~OrientationGenerator() {};
+};
+
+template <typename U>
+class RandomOrientationGenerator : public OrientationGenerator<U> {
+    public:
+        RandomOrientationGenerator() {;}
+        virtual void generateNext(Tensor2<U>& rotMatrix) {rotMatrix(0,0) = 9;}
+        
+    private:
+        
+};
 
 template <typename U>
 class Experiment {
@@ -90,8 +116,9 @@ class Experiment {
         U tStart;
         U tEnd;
         U strainRate;
-        std::function<hpp::Tensor2<U>(U)> F_of_t;
-        std::function<hpp::Tensor2<U>(U)> L_of_t;
+        std::function<Tensor2<U>(U)> F_of_t;
+        std::function<Tensor2<U>(U)> L_of_t;
+        std::shared_ptr<OrientationGenerator<U>> orientationGenerator;
 };
 
 } // END NAMESPACE hpp
--- src/casesUtils.cpp
+++ src/casesUtils.cpp
@@ -3,39 +3,39 @@
 
 namespace hpp 
 {
-    
+
 template <typename U>
 Experiment<U>::Experiment(std::string experimentName) {
+    // Common to most
+    tStart = 0.0;
+    orientationGenerator = std::make_shared<RandomOrientationGenerator<U>>();
+    
     if (experimentName == "kalidindi1992_simple_shear") {
-        tStart = 0.0;
         tEnd = 823.53;
         strainRate = -1.7e-3;
         F_of_t = std::bind(simpleShearDeformationGradient<U>, std::placeholders::_1, strainRate);
         L_of_t = std::bind(simpleShearVelocityGradient<U>, std::placeholders::_1, strainRate);
+        
     }
     else if (experimentName == "kalidindi1992_simple_compression") {
-        tStart = 0.0;
         tEnd = 1500.00;
         strainRate = -1.0e-3;
         F_of_t = std::bind(simpleCompressionDeformationGradient<U>, std::placeholders::_1, strainRate);
         L_of_t = std::bind(simpleCompressionVelocityGradient<U>, std::placeholders::_1, strainRate);
     }
     else if (experimentName == "savage2015_plane_strain_compression") {
-        tStart = 0.0;
         tEnd = 1000.0;
         strainRate = 1.0e-3;
         F_of_t = std::bind(planeStrainCompressionDeformationGradient<U>, std::placeholders::_1, strainRate);
         L_of_t = std::bind(planeStrainCompressionVelocityGradient<U>, std::placeholders::_1, strainRate);
     }
     else if (experimentName == "mihaila2014_simple_shear") {
-        tStart = 0.0;
         tEnd = 1000.0;
         strainRate = 1.0e-3;
         F_of_t = std::bind(simpleShearDeformationGradient<U>, std::placeholders::_1, strainRate);
         L_of_t = std::bind(simpleShearVelocityGradient<U>, std::placeholders::_1, strainRate);
     }
     else if (experimentName == "static") {
-        tStart = 0.0;
         tEnd = 1.0e-2;
         strainRate = 0.0;
         F_of_t = hpp::staticDeformationGradient<U>;
