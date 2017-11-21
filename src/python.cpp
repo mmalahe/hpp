88773c3ca9623bec423ccf54e1dce256ec47c98e
Michael Malahe <mmalahe@gmail.com>
2017-11-21 14:18:26 +0200

    Added initial class binding.

--- src/python.cpp
+++ src/python.cpp
@@ -3,6 +3,7 @@
 /// @brief The Python interface to the lbirary.
 
 #include <boost/python.hpp>
+#include <hpp/tensor.h>
 
 char const *firstMethod() {
     return "This is the first try.";
@@ -10,4 +11,8 @@ char const *firstMethod() {
 
 BOOST_PYTHON_MODULE(hpppy) {
     boost::python::def("getTryString", firstMethod);
+    // Tensor class
+    boost::python::class_<hpp::Tensor2<float>>("Tensor2F", 
+        boost::python::init<const unsigned int, const unsigned int>())
+        .def("getn1", &hpp::Tensor2<float>::getn1);
 }
