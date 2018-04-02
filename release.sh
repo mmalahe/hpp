b17e65ca55bcde1c1e02356903ca855fbaab1ac3
Michael Malahe <mmalahe@gmail.com>
2018-04-02 13:46:31 +0200

    Travis setup.

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 set -e
 mkdir -p build-release
 cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/mmalahe/local .. 
+cmake -DCMAKE_BUILD_TYPE=Release .. 
 cores=$(grep -c ^processor /proc/cpuinfo)
 make -j $cores
 cd ..
